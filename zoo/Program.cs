099d1badd742610917412b041221efd68f48f825
svobop <petr.svoboda@jumpshot.com>
2020-02-26 15:42:02 +0100

    add Lion class

--- zoo/Program.cs
+++ zoo/Program.cs
@@ -9,6 +9,13 @@ namespace zoo
         public string Gender;
         public int FedTimes;
 
+        public Animal()
+        {
+            this.Name = "unknown";
+            this.Age = 1;
+            this.Gender = "unknown";
+            this.FedTimes = 0;
+        }
         public Animal(string name, int age, string gender)
         {
             this.Name = name;
@@ -25,22 +32,45 @@ namespace zoo
             this.FedTimes = 0;
         }
 
-       // I didn't manage to write method that increases Fed.Times 
+        // I didn't manage to write method that increases Fed.Times 
+
 
         public string ToString()
         {
             return ($"{this.Name} is a {this.Age} years old {this.Gender} animal and was fed {this.FedTimes} times");
         }
+    }
 
+    public class Lion : Animal
+    {
+        public Lion(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+            this.FedTimes = 0;
+        }
 
+        public Lion(string name)
+        {
+            this.Name = name;
+            this.Age = 1;
+            this.Gender = "unknown";
+            this.FedTimes = 0;
+        }
+        public bool IsHungry()
+        {
+            return true;
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            var horse = new Animal("Roach");
-            Console.WriteLine(horse.ToString());
+            var lion = new Lion("Roach");
+            Console.WriteLine(lion.ToString());
+            Console.WriteLine(lion.IsHungry());
         }
     }
 }
