3303e471f3a87389a5ef5224ffff21b0ec75c379
svobop <petr.svoboda@jumpshot.com>
2020-02-26 14:56:43 +0100

    Animal class definition with constructors

--- zoo/Program.cs
+++ zoo/Program.cs
@@ -2,6 +2,30 @@
 
 namespace zoo
 {
+    public class Animal
+    {
+        public string Name;
+        public int Age;
+        public string Gender;
+        public int FeedTimes;
+
+        public Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public Animal(string name)
+        {
+            this.Name = name;
+            this.Age = 1;
+            this.Gender = "unknown";
+        }
+
+
+    }
+
     class Program
     {
         static void Main(string[] args)
